64f3c1bab17a738627aa05afaf3db9e560e9eee8
Krzysztof Rosa <krzysztof.rosa25@gmail.com>
2022-09-09 21:46:06 +0200

    pomysl taty

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 m = document.getElementById("lyf").getContext('2d')
 
+const MSIZE = 4000
+const G = 0.0000001
+
 draw = (x,y,c,c2,s) => {
     m.fillStyle = c
     m.fillRect(x,y,s,s)
@@ -17,7 +20,7 @@ drawcrc = (x,y,c,c2,s) => {
 drawvec = (x, y, vx, vy) => {
     m.beginPath()
     m.moveTo(x, y)
-    m.lineTo(x + vx, y + vy)
+    m.lineTo(x + 5 * vx, y + 5 * vy)
     m.strokeStyle = "white"
     m.stroke()
 }
@@ -25,67 +28,55 @@ drawvec = (x, y, vx, vy) => {
 
 particles = []
 particle = (x,y,c,c2,s,vx,vy) => { 
-    return { x: x, y: y, vx: vx, vy: vy, color: c, color2: c2, size: s}
+    return { x: x, y: y, vx: vx, vy: vy, ax: 0, ay: 0, color: c, color2: c2, size: s, m: Math.PI*s*s}
 }
 
 
-random = () => {
-    return Math.round(Math.random()*3900+50)
+random = (min, max) => {
+    return Math.round(Math.random()*max+min)
 }
 
-create = (number, color, color2, size, x, y, vx, vy) => {
-    group = []
-    for (i=0; i<number; i++) {
-        
-        group.push(particle(x || random(), y || random(), color, color2, size, vx || 0, vy || 0))
-        particles.push(group[i])
+create = (count, color, color2, size, x, y, vx, vy) => {
+    for (i=0; i<count; i++) {
+        particles.push(particle(x || random(size, MSIZE-size), y || random(size, MSIZE-size), color, color2, random(20,50), vx || 0, vy || 0))
     }
-    return group
 }
 // T H E  G R A V I T Y  M U L T I P L I E R
 // T H E  G R A V I T Y  M U L T I P L I E R
 
-gravity = (particles1, particles2) => {
-    for (i=0; i < particles1.length; i++) {
-        fx = 0
-        fy = 0
-        for (j=0; j < particles2.length; j++) {
-            a = particles1[i]
-            b = particles2[j]
-            dx = a.x - b.x
-            dy = a.y - b.y
-
-                //odleglosc^2
-            d = dx * dx + dy * dy
-            
-            if (d > 0) {
-                // f = ma
-                F = (a.size * b.size) / d
-
-                fx = (F * dx)
-                fy = (F * dy)
-            }
-        }
-        //      kms
-        a.vx = (a.vx + fx)
-        a.vy = (a.vy + fy)
-        a.x += a.vx * b.size
-        a.y += a.vy * b.size
+updatePhysics = () => {
+    for (let i=0; i < particles.length; i++) {
         
-        b.vx = (b.vx + fx)
-        b.vy = (b.vy + fy)
-        b.x += b.vx * a.size
-        b.y += b.vy * a.size
+        let p = particles[i]
+        let fx = 0
+        let fy = 0
 
-        if(a.x <= 1 || a.x >= 3999) {a.vx *= -1}
-        if(a.y <= 1 || a.y >= 3999) {a.vy *= -1}
-        if(b.x <= 1 || b.x >= 3999) {b.vx *= -1}
-        if(b.y <= 1 || b.y >= 3999) {b.vy *= -1}
-        
-        if(d <= b.size + a.size) {
+        for (let j=0; j<particles.length; j++) {
             
+            let p2 = particles[j]
+            
+            if (i===j) {continue}
+            
+            dx = (p.x - p2.x)
+            dy = (p.y - p2.y)
+            d = Math.sqrt(dx * dx + dy * dy)
+            f = G * (p.m * p2.m) / d^2
+            fx -= dx * f
+            fy -= dy * f
         }
+        
+        p.ax = fx / p.m
+        p.ay = fy / p.m
+        
+        //ruch ? 
+        p.vx += p.ax
+        p.vy += p.ay
+        p.x += p.vx
+        p.y += p.vy
+    
+        
     }
+
 }
 
 
@@ -93,33 +84,33 @@ gravity = (particles1, particles2) => {
 
 //red = create(1, ["#f00", "#400"], 5)
 //green = create(1, ["#0f0", "#040"], 2)
-orange = create(1, "#ffb226", "#f48225", 40, 1000, 1000, 0, 100)
-blue = create(1, "#22a6f2", "#1771a5", 100, 3000, 3000, 0, 0)
+create(5, "#ffb226", "#f48225", 12, null, null, 0, 0)
+//create("#22a6f2", "#1771a5", 100, 3000, 3000, 0, 0)
 
 
 update=()=>{
-
+    
+    updatePhysics()
     //   -- Rules --
-    gravity(blue, orange)
 // particles[0].x = 2000
 // particles[0].y = 2000
     //  -- The End --
 
-    m.clearRect(0, 0, 4000, 4000)
+    m.clearRect(0, 0, MSIZE, MSIZE)
     m.fillStyle = "black"
-    m.fillRect(0, 0, 4000, 4000)
+    m.fillRect(0, 0, MSIZE, MSIZE)
     for (i=0; i<particles.length; i++) {
         drawcrc(particles[i].x, particles[i].y, particles[i].color, particles[i].color2, particles[i].size)
         drawvec(particles[i].x, particles[i].y, particles[i].vx, particles[i].vy)
     }
     requestAnimationFrame(update)
-    velocity = Math.sqrt(particles[1].vx * particles[1].vx + particles[1].vy * particles[1].vy)
+    //velocity = Math.sqrt(particles[1].vx * particles[1].vx + particles[1].vy * particles[1].vy)
     
-    divc.innerHTML = "velocity: " + velocity.toFixed(3)
+    //divc.innerHTML = "velocity: " + velocity.toFixed(3)
 }
- div = document.getElementById("box")
- divc = document.createElement("div")
- div.append(divc)
+//  div = document.getElementById("box")
+//  divc = document.createElement("div")
+//  div.append(divc)
 // setInterval(() => {
 //     update();
 // },700);
